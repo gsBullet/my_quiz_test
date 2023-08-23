04e0839299b7641f08997c396bb3365263eff60f
Golam Sarowar <86582538+gsBullet@users.noreply.github.com>
2023-08-23 21:14:42 +0600

    frontend setup

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require("express");
 const server = express();
 const port = 5000;
 
+
+server.set('view engine', 'ejs');
+server.set('views',  './views');
+
+server.use(express.static('public'))
+
+
 server.get('/', (req,res,next)=>{
-    res.send('Hello from express');
+    res.render('frontend/index.ejs');
 })
 
 server.listen(port, ()=>{
