47689d031d184aa44ed06a96c7b7d6334b6d1a0b
Golam Sarowar <86582538+gsBullet@users.noreply.github.com>
2023-08-26 17:06:59 +0600

    update & edit complete

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const server = express();
 const fs = require('fs');
 const read_json = require('./database/read_json');
 const write_json = require('./database/write_json');
+const { title } = require("process");
+const { log } = require("console");
 const port = 5000;
 
 
@@ -22,21 +24,57 @@ server.use(express.static('public'));
 server.get('/', (req, res, next) => {
     const filePath = './database/todo.json';
     let todo_list = read_json(filePath);
-res.render('frontend/index.ejs',{
-    todo_list
-});
+    res.render('frontend/index.ejs', {
+        todo_list
+    });
 })
-server.post('/form-info', (req, res, next) => {
-    console.log(req.body);
-    
-    const {todo_title} = req.body;
+server.get('/edit/:index', (req, res, next) => {
     const filePath = './database/todo.json';
-   let todo_list = read_json(filePath);
+    let todo_list = read_json(filePath);
+    // console.log(req.params.index);
+    // console.log(todo_list);
+    let item = todo_list[req.params.index];
+    if(item){
+        res.render('frontend/edit.ejs',{
+            todo: item,
+            index: req.params.index
+        });
+
+    }
+    else{
+        res.send('404 not found');
+    }
+    
+})
+server.post('/update-form-info',(req,res)=>{
+    const filePath = './database/todo.json';
+    let todo_list = read_json(filePath);
+    let item = todo_list[req.body.index];
+    console.log(item);
+    if(item){
+        item.title= req.body.todo_title;
+        write_json(filePath, todo_list);
+
+        return res.redirect('/');
+    }
+    else{
+        res.send('todo list not updated');
+    }
+    console.log(todo_list);
+})
+
+server.post('/form-info', (req, res, next) => {
+   
+    const {
+        todo_title
+    } = req.body;
+    const filePath = './database/todo.json';
+    let todo_list = read_json(filePath);
     todo_list.push({
         title: todo_title,
         is_position: 0
     });
-    write_json(filePath,todo_list);
+    write_json(filePath, todo_list);
 
     // console.log(todo_list);
     // res.send(JSON.stringify(todo_list));
